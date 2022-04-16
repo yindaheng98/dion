5ad644f7c6f96cac19ea45ff9c0e6fd47bb80307
yindaheng98 <yindaheng98@gmail.com>
2022-04-16 21:02:54 +0800

    interface modify

--- algorithms/interfaces.go
+++ algorithms/interfaces.go
@@ -4,14 +4,9 @@ import pb "github.com/yindaheng98/isglb/proto"
 
 type Algorithm interface {
 
-	// UpdateSFUStatus tell the algorithm that the SFU graph has changed
-	// The output is that which SFU's status should change
-	UpdateSFUStatus(*pb.SFUStatus) []*pb.SFUStatus
-
-	// UpdateQuality tell the algorithm of the computation and communication quality
-	// The output is that which SFU's status should change
-	UpdateQuality(*pb.Report) []*pb.SFUStatus
-
-	// GetSFUStatus get the expected SFU status from the algorithm
-	GetSFUStatus(string) *pb.SFUStatus
+	// UpdateSFUStatus tell the algorithm that the SFU graph and the computation and communication quality has changed
+	// `current` is the changed SFU's current status
+	// `reports` is a series of Quality Report
+	// `expected` is that which SFU's status should change
+	UpdateSFUStatus(current []*pb.SFUStatus, reports []*pb.QualityReport) (expected []*pb.SFUStatus)
 }
