76a0580abb12fe992e708bb76c21639db48c1526
yindaheng98 <yindaheng98@gmail.com>
2022-04-17 15:20:58 +0800

    a random alg for test

---
+++ algorithms/impl/random/Random.go
@@ -0,0 +1,121 @@
+package random
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/pion/ion/pkg/util"
+	"github.com/pion/ion/proto/ion"
+	"github.com/yindaheng98/isglb/algorithms"
+	pb "github.com/yindaheng98/isglb/proto"
+	"math/rand"
+)
+
+type Random struct {
+	algorithms.Algorithm
+}
+
+func RandBool() bool {
+	return rand.Intn(2) == 0
+}
+
+func RandNode(nid string) *ion.Node {
+	return &ion.Node{
+		Dc:      util.RandomString(2),
+		Nid:     nid,
+		Service: util.RandomString(4),
+		Rpc: &ion.RPC{
+			Protocol: util.RandomString(4),
+			Addr:     util.RandomString(8),
+		},
+	}
+}
+
+func (Random) UpdateSFUStatus(current []*pb.SFUStatus, reports []*pb.QualityReport) (expected []*pb.SFUStatus) {
+	fmt.Printf("┎Received status: %+v\n", current)
+	fmt.Printf("┖Received report: %+v\n", reports)
+	for _, s := range current {
+		if RandBool() || RandBool() {
+			expected = append(expected, s)
+		}
+		if !RandBool() {
+			continue
+		}
+
+		for _, t := range s.ForwardTracks {
+			if !RandBool() {
+				continue
+			}
+			if RandBool() {
+				t.TrackId = util.RandomString(4)
+			}
+			if RandBool() {
+				t.Src = RandNode(t.Src.Nid)
+			}
+		}
+		if RandBool() {
+			s.ForwardTracks = append(s.ForwardTracks, &pb.ForwardTrack{
+				Src:     RandNode(util.RandomString(1)),
+				TrackId: util.RandomString(4),
+			})
+		}
+
+		for _, t := range s.ProceedTracks {
+			if !RandBool() {
+				continue
+			}
+			if RandBool() {
+				t.SrcTrackId = util.RandomString(4)
+			}
+			if RandBool() {
+				t.DstTrackId = util.RandomString(4)
+			}
+			if RandBool() {
+				t.Procedure = util.RandomString(4)
+			}
+		}
+		if RandBool() {
+			s.ProceedTracks = append(s.ProceedTracks, &pb.ProceedTrack{
+				SrcTrackId: util.RandomString(4),
+				DstTrackId: util.RandomString(4),
+				Procedure:  util.RandomString(2),
+			})
+		}
+	}
+
+	if !RandBool() {
+		expected = append(expected, &pb.SFUStatus{
+			SFU: RandNode(util.RandomString(1)),
+		})
+	}
+
+	return
+}
+
+type RandReports struct {
+	reports []*pb.QualityReport
+}
+
+func (r *RandReports) RandReports() (reports []*pb.QualityReport) {
+	for _, report := range r.reports {
+		if RandBool() || RandBool() {
+			reports = append(reports, report)
+		}
+		if RandBool() {
+			continue
+		}
+		if RandBool() {
+			t := &timestamp.Timestamp{}
+			t.Seconds = rand.Int63()
+			report.Timestamp = t
+		}
+	}
+	if RandBool() {
+		t := &timestamp.Timestamp{}
+		t.Seconds = rand.Int63()
+		reports = append(reports, &pb.QualityReport{
+			Timestamp: t,
+		})
+	}
+	r.reports = reports
+	return
+}
---
+++ algorithms/impl/random/Random_test.go
@@ -0,0 +1,23 @@
+package random
+
+import (
+	"fmt"
+	pb "github.com/yindaheng98/isglb/proto"
+	"testing"
+)
+
+func TestRandom_UpdateSFUStatus(t *testing.T) {
+	alg := &Random{}
+	rr := &RandReports{}
+	lst := make([]*pb.SFUStatus, 0)
+	for i := 0; i < 100; i++ {
+		lst = alg.UpdateSFUStatus(lst, rr.RandReports())
+	}
+}
+
+func TestRandReports_RandReports(t *testing.T) {
+	rr := &RandReports{}
+	for i := 0; i < 100; i++ {
+		fmt.Printf("%+v\n", rr.RandReports())
+	}
+}
