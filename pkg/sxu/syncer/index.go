6eca6d537b66c4d29a58d1e6c76d08cae46a8a44
yindaheng98 <yindaheng98@gmail.com>
2022-04-21 11:51:17 +0800

    Advance abstract

--- pkg/sxu/syncer/index.go
+++ pkg/sxu/syncer/index.go
@@ -1,87 +1,115 @@
 package syncer
 
 import (
-	pb "github.com/yindaheng98/isglb/proto"
+	"sort"
+	"strings"
 )
 
-// TODO: Keep Order
+type IndexData interface {
+	// Key return the key of this item
+	Key() string
+	// Compare compare if two IndexData is the same
+	Compare(IndexData) bool
+}
 
-type forwardedTrackTuple struct {
+type indexTuple struct {
+	data    IndexData
 	checked bool
-	track   *pb.ForwardTrack
 }
-type forwardedTrackSet map[string]*forwardedTrackTuple // set<trackId>
-type forwardedTrackIndex map[string]forwardedTrackSet  // map<src.NID, set<trackId>>
 
-// resetCheck set all the bool value in forwardedTrackIndex to be false
-// so you can compare it with those in SFUStatus find which is useless
-func (index forwardedTrackIndex) resetCheck() {
-	for _, set := range index {
-		for _, forwardTrack := range set {
-			forwardTrack.checked = false
-		}
+type Index struct {
+	index map[string]*indexTuple
+}
+
+func (index *Index) reset() {
+	for _, tuple := range index.index {
+		tuple.checked = false
 	}
 }
 
-// construct build a []*pb.ForwardTrack for SFUStatus
-func (index forwardedTrackIndex) construct() []*pb.ForwardTrack {
-	total := 0
-	for _, set := range index {
-		for range set {
-			total += 1
+// Add add a data
+func (index *Index) Add(data IndexData) {
+	index.index[data.Key()] = &indexTuple{
+		data:    data,
+		checked: false,
+	}
+}
+
+// Del delete a data
+func (index *Index) Del(data IndexData) {
+	delete(index.index, data.Key())
+}
+
+// Exist check if a data exists
+func (index *Index) Exist(data IndexData) bool {
+	if tuple, ok := index.index[data.Key()]; ok {
+		return tuple.data.Compare(data)
+	}
+	return false
+}
+
+// Update update the index
+// and shows the difference between index and the input list
+func (index *Index) Update(list []IndexData) (add []IndexData, del []IndexData) {
+	index.reset()
+
+	// Find those data in input list but not in index, add it or replace it
+	for _, data := range list {
+		if tuple, ok := index.index[data.Key()]; ok { // Check if the key exists
+			// Exits?
+			if tuple.data.Compare(data) { // Compare the value
+				// Also same?
+				tuple.checked = true // do nothing
+			} else { // Value not same?
+				del = append(del, tuple.data) // delete the exists
+				add = append(add, data)       // add the new
+				tuple.data = data             // replace it
+				tuple.checked = true
+			}
+		} else { //key not exists?
+			add = append(add, data) // just add the new
+			index.index[data.Key()] = &indexTuple{
+				data:    data,
+				checked: true,
+			}
 		}
 	}
 
-	forwardTracks := make([]*pb.ForwardTrack, total)
+	// Find those data in index but not in input list, delete it
+	for key, tuple := range index.index {
+		if !tuple.checked { // not exists in input list?
+			del = append(del, tuple.data) // delete it
+			delete(index.index, key)
+		}
+	}
+
+	return
+}
+
+type indexDataList []IndexData
+
+func (s indexDataList) Len() int {
+	return len(s)
+}
+
+func (s indexDataList) Less(i, j int) bool {
+	return strings.Compare(s[i].Key(), s[j].Key()) < 0
+}
+
+func (s indexDataList) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+// Gather gather all the data and output it in key order
+func (index *Index) Gather() []IndexData {
+	var list = make(indexDataList, len(index.index))
 	i := 0
-	for _, set := range index {
-		for _, forwardTrack := range set {
-			forwardTracks[i] = forwardTrack.track
-			i += 1
-		}
+	for _, d := range index.index {
+		list[i] = d.data
+		i++
 	}
-	return forwardTracks
+	sort.Sort(list)
+	return list
 }
 
-type proceededTrackTuple struct {
-	checked bool
-	track   *pb.ProceedTrack
-}
-type proceededTrackIndex map[string]*proceededTrackTuple // map<dstTrackId, track>
-
-// resetCheck set all the bool value in proceededTrackIndex to be false
-// so you can compare it with those in SFUStatus find which is useless
-func (index proceededTrackIndex) resetCheck() {
-	for _, proceedTrackTuple := range index {
-		proceedTrackTuple.checked = false
-	}
-}
-
-// construct build a []*pb.ProceedTrack for SFUStatus
-func (index proceededTrackIndex) construct() []*pb.ProceedTrack {
-	var proceedTracks []*pb.ProceedTrack
-	for _, proceedTrackTuple := range index {
-		proceedTracks = append(proceedTracks, proceedTrackTuple.track)
-	}
-	return proceedTracks
-}
-
-type clientSideSessionTuple struct { //tuple<ClientNeededSession.String(), ClientNeededSession>
-	checked bool
-	session *pb.ClientNeededSession
-}
-type clientSideSessionIndex map[string]clientSideSessionTuple
-
-func (index clientSideSessionIndex) resetCheck() {
-	for _, clientSessionTuple := range index {
-		clientSessionTuple.checked = false
-	}
-}
-
-func (index clientSideSessionIndex) construct() []*pb.ClientNeededSession {
-	var clientSessions []*pb.ClientNeededSession
-	for _, clientSessionTuple := range index {
-		clientSessions = append(clientSessions, clientSessionTuple.session)
-	}
-	return clientSessions
-}
+// TODO: Cache the Gather(), if the index not same, just return the cache
