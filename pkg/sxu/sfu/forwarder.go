3bdcb3c6cab8c41b1bb601d21d308cbed253629a
yindaheng98 <yindaheng98@gmail.com>
2022-05-09 16:39:37 +0800

    simplify

--- pkg/sxu/sfu/forwarder.go
+++ pkg/sxu/sfu/forwarder.go
@@ -2,13 +2,8 @@ package sfu
 
 import (
 	"context"
-	log "github.com/pion/ion-log"
-	ion_sfu "github.com/pion/ion-sfu/pkg/sfu"
-	pbrtc "github.com/pion/ion/proto/rtc"
-	"github.com/yindaheng98/dion/pkg/sxu/rtc"
 	pb "github.com/yindaheng98/dion/proto"
 	"github.com/yindaheng98/dion/util"
-	"google.golang.org/grpc/metadata"
 	"time"
 )
 
@@ -16,23 +11,19 @@ const RetryInterval time.Duration = time.Second * 1
 
 type Track struct {
 	util.ForwardTrackItem
-	ctx      context.Context
 	cancel   context.CancelFunc
 	updateCh chan util.ForwardTrackItem
 }
 
 type ForwardController struct {
-	sfu      *ion_sfu.SFU
-	client   pbrtc.RTCClient
-	tracks   map[string]*Track
-	Metadata metadata.MD
+	ForwardTrackRoutineFactory
+	tracks map[string]*Track
 }
 
-func NewForwarder(sfu *ion_sfu.SFU, client pbrtc.RTCClient) *ForwardController {
+func NewForwardController(factory ForwardTrackRoutineFactory) *ForwardController {
 	return &ForwardController{
-		sfu:    sfu,
-		client: client,
-		tracks: make(map[string]*Track),
+		ForwardTrackRoutineFactory: factory,
+		tracks:                     make(map[string]*Track),
 	}
 }
 
@@ -43,80 +34,15 @@ func (f *ForwardController) StartForwardTrack(trackInfo *pb.ForwardTrack) {
 		return
 	}
 	ctx, cancel := context.WithCancel(context.Background())
+	updateCh := make(chan util.ForwardTrackItem, 1)
+	updateCh <- item
 	track := &Track{
 		ForwardTrackItem: item,
-		ctx:              ctx,
 		cancel:           cancel,
-		updateCh:         make(chan util.ForwardTrackItem, 1),
+		updateCh:         updateCh,
 	}
 	f.tracks[track.Key()] = track
-	go f.forwardTrackRoutine(track) // One thread pre track
-}
-
-func newRTC(track *Track, sfu *ion_sfu.SFU) (*rtc.RTC, context.Context, context.CancelFunc) {
-	ctx, cancel := context.WithCancel(track.ctx)
-	r := rtc.NewRTC(sfu)
-	r.OnError = func(err error) {
-		_ = r.Close() // Close
-		cancel()      // Close
-		select {
-		case <-track.ctx.Done(): // this track should exit?
-			return // just exit
-		default: // should not exit?
-			log.Errorf("Forwarding exited with an error: %+v", err) // should retry
-		}
-	}
-	return r, ctx, cancel
-}
-
-// forwardTrackRoutine retry until success
-// !!!SINGLE THREAD for each Track!!!
-func (f *ForwardController) forwardTrackRoutine(track *Track) {
-	for {
-		r, ctx, cancel := newRTC(track, f.sfu)
-		err := r.Start(track.Track.RemoteSessionId, track.Track.LocalSessionId, f.client, f.Metadata)
-		if err != nil { // if error
-			_ = r.Close() // Close
-			cancel()      // Close
-			select {
-			case <-track.ctx.Done(): // this track should exit?
-				return // exit
-			case <-time.After(RetryInterval): // this track should not exit
-				log.Errorf("Error when starting forward a track, retry it: %+v", err)
-				continue // retry
-			}
-		}
-		// Start successfully, the start updating
-		retryItemCh := make(chan util.ForwardTrackItem, 1)
-		for {
-			var item util.ForwardTrackItem
-			select {
-			case <-ctx.Done(): // some error occurred? updating should not continue
-				return
-			case item = <-track.updateCh: // get item from update channel or retry channel
-			case item = <-retryItemCh: // get item from update channel or retry channel
-			}
-			if r.IsSame(item.Track) { // If is same
-				continue // Just skip
-			}
-			log.Debugf("Updating track: %+v", item.Track)
-			err := r.Update(item.Track) // Update it
-			if err != nil {
-				select {
-				case <-ctx.Done(): // Error occurred? updating should not continue
-					return
-				default: // should retry
-					log.Errorf("Error updating track, retry it: %+v", err)
-					retryItemCh <- item
-					select {
-					case <-ctx.Done(): // Error occurred? updating should not continue
-						return
-					case <-time.After(RetryInterval): // Delay to retry
-					}
-				}
-			}
-		}
-	}
+	go f.ForwardTrackRoutine(ctx, updateCh) // One thread pre track
 }
 
 func (f *ForwardController) StopForwardTrack(trackInfo *pb.ForwardTrack) {
--- pkg/sxu/sfu/interfaces.go
+++ pkg/sxu/sfu/interfaces.go
@@ -1,51 +1,10 @@
 package sfu
 
 import (
-	log "github.com/pion/ion-log"
-	sdk "github.com/pion/ion-sdk-go"
-	ion_sfu "github.com/pion/ion-sfu/pkg/sfu"
-	"github.com/pion/ion/pkg/ion"
-	pb "github.com/yindaheng98/dion/proto"
+	"context"
+	"github.com/yindaheng98/dion/util"
 )
 
-func NewIONSFU(conf ion_sfu.Config) *ion_sfu.SFU {
-	return ion_sfu.NewSFU(conf)
-}
-
-type SFUTrackRouter struct {
-	node              *ion.Node
-	connectors        map[string]*sdk.Connector
-	sfu               *ion_sfu.SFU
-	forwardController ForwardController
-	proceedController ProceedController
-}
-
-// TODO: All the methods should retry when failed until success
-
-func (s *SFUTrackRouter) StartForwardTrack(trackInfo *pb.ForwardTrack) {
-	err := s.forwardController.StartTransmit(trackInfo)
-	if err != nil {
-		log.Errorf("negotiation error: %v", err)
-	}
-	// TODO: Infinity retry until call StopForwardTrack or ReplaceForwardTrack
-}
-
-func (s *SFUTrackRouter) StopForwardTrack(trackInfo *pb.ForwardTrack) {
-	panic("implement me")
-}
-
-func (s *SFUTrackRouter) ReplaceForwardTrack(oldTrackInfo *pb.ForwardTrack, newTrackInfo *pb.ForwardTrack) {
-	panic("implement me")
-}
-
-func (s *SFUTrackRouter) StartProceedTrack(trackInfo *pb.ProceedTrack) {
-	panic("implement me")
-}
-
-func (s *SFUTrackRouter) StopProceedTrack(trackInfo *pb.ProceedTrack) {
-	panic("implement me")
-}
-
-func (s *SFUTrackRouter) ReplaceProceedTrack(oldTrackInfo *pb.ProceedTrack, newTrackInfo *pb.ProceedTrack) {
-	panic("implement me")
+type ForwardTrackRoutineFactory interface {
+	ForwardTrackRoutine(ctx context.Context, updateCh <-chan util.ForwardTrackItem)
 }
