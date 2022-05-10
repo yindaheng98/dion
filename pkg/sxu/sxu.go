7b51b78515be39b38cf1ed6ea1203554cd70e87b
yindaheng98 <yindaheng98@gmail.com>
2022-05-10 18:16:28 +0800

    move to sxu

--- pkg/sxu/sxu.go
+++ pkg/sxu/sxu.go
@@ -1,14 +1,21 @@
 package sxu
 
 import (
+	"github.com/cloudwebrtc/nats-discovery/pkg/discovery"
+	nrpc "github.com/cloudwebrtc/nats-grpc/pkg/rpc"
+	"github.com/cloudwebrtc/nats-grpc/pkg/rpc/reflection"
 	log "github.com/pion/ion-log"
 	ion_sfu_log "github.com/pion/ion-sfu/pkg/logger"
 	ion_sfu "github.com/pion/ion-sfu/pkg/sfu"
 	"github.com/pion/ion/pkg/ion"
+	"github.com/pion/ion/pkg/node/sfu"
+	"github.com/pion/ion/pkg/proto"
+	"github.com/pion/ion/pkg/runner"
 	pbion "github.com/pion/ion/proto/ion"
-	"github.com/yindaheng98/dion/pkg/sxu/sfu"
+	pb "github.com/pion/ion/proto/rtc"
 	"github.com/yindaheng98/dion/pkg/sxu/syncer"
 	"github.com/yindaheng98/dion/util"
+	"google.golang.org/grpc"
 )
 
 const ServiceSXU = "sxu"
@@ -27,17 +34,27 @@ type Config struct {
 
 type SXU struct {
 	ion.Node
-	syncer *syncer.ISGLBSyncer
-	server *sfu.SFUServer
-	isfu   *ion_sfu.SFU
-	conf   Config
+	s *sfu.SFUService
+	runner.Service
+	conf Config
+
+	sfu *ion_sfu.SFU
+
+	toolbox ToolBoxBuilder
+	syncer  *syncer.ISGLBSyncer
 }
 
-func NewSXU() *SXU {
+func NewSXU(toolbox ToolBoxBuilder) *SXU {
+	if toolbox == nil {
+		toolbox = DefaultToolBoxBuilder{}
+	}
 	return &SXU{
-		Node: ion.NewNode("sxu-" + util.RandomString(8)),
+		Node:    ion.NewNode("sxu-" + util.RandomString(8)),
+		toolbox: toolbox,
 	}
 }
+
+// ↓↓↓↓↓ COPY FROM https://github.com/pion/ion/blob/65dbd12eaad0f0e0a019b4d8ee80742930bcdc28/pkg/node/sfu/sfu.go ↓↓↓↓↓
 
 // Load load config file
 func (s *SXU) Load(confFile string) error {
@@ -49,20 +66,63 @@ func (s *SXU) Load(confFile string) error {
 	return nil
 }
 
-func (s *SXU) Start(conf Config) error {
-	return s.StartWithBuilder(conf, DefaultToolBoxBuilder{})
+// StartGRPC start with grpc.ServiceRegistrar
+func (s *SXU) StartGRPC(registrar grpc.ServiceRegistrar) error {
+	//s.s = sfu.NewSFUService(s.conf.Config)
+	s.s = sfu.NewSFUServiceWithSFU(s.sfu)
+	pb.RegisterRTCServer(registrar, s.s)
+	log.Infof("sfu pb.RegisterRTCServer(registrar, s.s)")
+	return nil
 }
 
-func (s *SXU) StartWithBuilder(conf Config, toolbox ToolBoxBuilder) error {
-	// Start internal SFU
-	s.isfu = ion_sfu.NewSFU(conf.ISFU)
+// Start sfu server
+func (s *SXU) Start(conf Config) error {
+	// ↑↑↑↑↑ COPY FROM https://github.com/pion/ion/blob/65dbd12eaad0f0e0a019b4d8ee80742930bcdc28/pkg/node/sfu/sfu.go ↑↑↑↑↑
 
-	// Start SFU Signal service
-	s.server = sfu.NewSFUServer(&s.Node, s.isfu)
-	err := s.server.Start(conf.Config)
+	// Start internal SFU
+	s.sfu = ion_sfu.NewSFU(conf.ISFU)
+
+	// ↓↓↓↓↓ COPY FROM https://github.com/pion/ion/blob/65dbd12eaad0f0e0a019b4d8ee80742930bcdc28/pkg/node/sfu/sfu.go ↓↓↓↓↓
+	err := s.Node.Start(conf.Nats.URL)
 	if err != nil {
+		s.Close()
 		return err
 	}
+
+	//s.s = sfu.NewSFUService(conf.Config)
+	s.s = sfu.NewSFUServiceWithSFU(s.sfu)
+	//grpc service
+	pb.RegisterRTCServer(s.Node.ServiceRegistrar(), s.s)
+
+	// Register reflection service on nats-rpc server.
+	reflection.Register(s.Node.ServiceRegistrar().(*nrpc.Server))
+
+	node := discovery.Node{
+		DC:      conf.Global.Dc,
+		Service: ServiceSXU, //proto.ServiceRTC,
+		NID:     s.Node.NID,
+		RPC: discovery.RPC{
+			Protocol: discovery.NGRPC,
+			Addr:     conf.Nats.URL,
+			//Params:   map[string]string{"username": "foo", "password": "bar"},
+		},
+	}
+
+	go func() {
+		err := s.Node.KeepAlive(node)
+		if err != nil {
+			log.Errorf("sfu.Node.KeepAlive(%v) error %v", s.Node.NID, err)
+		}
+	}()
+
+	//Watch ALL nodes.
+	go func() {
+		err := s.Node.Watch(proto.ServiceALL)
+		if err != nil {
+			log.Errorf("Node.Watch(proto.ServiceALL) error %v", err)
+		}
+	}()
+	// ↑↑↑↑↑ COPY FROM https://github.com/pion/ion/blob/65dbd12eaad0f0e0a019b4d8ee80742930bcdc28/pkg/node/sfu/sfu.go ↑↑↑↑↑
 
 	// Start syncer
 	s.syncer = syncer.NewSFUStatusSyncer(&s.Node, "*", &pbion.Node{
@@ -70,14 +130,17 @@ func (s *SXU) StartWithBuilder(conf Config, toolbox ToolBoxBuilder) error {
 		Nid:     s.Node.NID,
 		Service: ServiceSXU,
 		Rpc:     nil,
-	}, toolbox.Build(&s.Node, s.isfu))
+	}, s.toolbox.Build(&s.Node, s.sfu))
 	s.syncer.Start()
 
+	// ↓↓↓↓↓ COPY FROM https://github.com/pion/ion/blob/65dbd12eaad0f0e0a019b4d8ee80742930bcdc28/pkg/node/sfu/sfu.go ↓↓↓↓↓
 	return nil
 }
 
 // Close all
 func (s *SXU) Close() {
+	s.Node.Close()
 	s.syncer.Stop()
-	s.server.Close()
 }
+
+// ↑↑↑↑↑ COPY FROM https://github.com/pion/ion/blob/65dbd12eaad0f0e0a019b4d8ee80742930bcdc28/pkg/node/sfu/sfu.go ↑↑↑↑↑
