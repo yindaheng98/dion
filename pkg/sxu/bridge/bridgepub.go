8db0d93ca09625fd7695b4b722d8e399d88957b5
yindaheng98 <yindaheng98@gmail.com>
2022-05-11 22:04:18 +0800

    use a common struct

--- pkg/sxu/bridge/bridgepub.go
+++ pkg/sxu/bridge/bridgepub.go
@@ -1,69 +1,18 @@
 package bridge
 
 import (
-	"context"
 	log "github.com/pion/ion-log"
 	ion_sfu "github.com/pion/ion-sfu/pkg/sfu"
 	"github.com/pion/ion/proto/rtc"
 	"github.com/pion/webrtc/v3"
 )
 
-func candidateSetting(pc *webrtc.PeerConnection, peer *ion_sfu.PeerLocal, errCh chan<- error, Target rtc.Target) {
-	var pcCand []webrtc.ICECandidateInit // Store unsended ICECandidate
-	peer.OnIceCandidate = func(candidate *webrtc.ICECandidateInit, target int) {
-		if target != int(Target) { // detect target
-			return // I do not want other's candidate
-		}
-		if pc.CurrentRemoteDescription() == nil { // If not initialized
-			pcCand = append(pcCand, *candidate) // just store it
-			return
-		}
-		// initialized
-		tpcCand := pcCand
-		pcCand = []webrtc.ICECandidateInit{} // Clear it
-		// And add them all
-		tpcCand = append(tpcCand, *candidate)
-		for _, c := range tpcCand {
-			err := pc.AddICECandidate(c)
-			if err != nil {
-				log.Errorf("Cannot add ICECandidate: %+v", err)
-				select {
-				case errCh <- err:
-				default:
-				}
-				return
-			}
-		}
-	}
-	pc.OnICECandidate(func(candidate *webrtc.ICECandidate) {
-		// Just do it, peer can dealing with Stable state
-		if candidate == nil {
-			return
-		}
-		err := peer.Trickle(candidate.ToJSON(), int(Target))
-		if err != nil {
-			log.Errorf("Cannot Trickle: %+v", err)
-			select {
-			case errCh <- err:
-			default:
-			}
-			return
-		}
-	})
-}
-
 type Publisher struct {
-	peer    *ion_sfu.PeerLocal
-	pc      *webrtc.PeerConnection
-	errCh   chan error
-	ctx     context.Context
-	cancel  context.CancelFunc
-	OnClose func(err error)
+	bridgePeer
 }
 
 func NewPublisher(peer *ion_sfu.PeerLocal, pc *webrtc.PeerConnection) Publisher {
-	ctx, cancel := context.WithCancel(context.Background())
-	return Publisher{peer: peer, pc: pc, errCh: make(chan error, 16), ctx: ctx, cancel: cancel}
+	return Publisher{bridgePeer: newPeer(peer, pc)}
 }
 
 // Publish publish PeerConnection to PeerLocal.Subscriber
@@ -90,7 +39,7 @@ func (p Publisher) Publish(sid string) error {
 		}
 		answer, err := p.peer.Answer(offer)
 		if err != nil {
-			log.Errorf("Cannot create Answer in peer: %+v", err)
+			log.Errorf("Cannot create Answer in bridgePeer: %+v", err)
 			select {
 			case errCh <- err:
 			default:
@@ -123,38 +72,12 @@ func (p Publisher) Publish(sid string) error {
 	return err
 }
 
-func (p Publisher) logger() {
-	for {
-		select {
-		case err := <-p.errCh:
-			p.close(err)
-		case <-p.ctx.Done():
-			p.close(nil)
-		}
-	}
-}
-
 func (p Publisher) AddTrack(track webrtc.TrackLocal) (*webrtc.RTPSender, error) {
 	addTrack, err := p.pc.AddTrack(track)
 	if err != nil {
 		return nil, err
 	}
 	return addTrack, nil
-}
-
-func (p Publisher) close(err0 error) {
-	p.cancel()
-	err := p.peer.Close()
-	if err != nil {
-		log.Errorf("Error when closing peer in publisher: %+v", err)
-	}
-	err = p.pc.Close()
-	if err != nil {
-		log.Errorf("Error when closing pc in publisher: %+v", err)
-	}
-	if p.OnClose != nil {
-		p.OnClose(err0)
-	}
 }
 
 func (p Publisher) Close() {
---
+++ pkg/sxu/bridge/common.go
@@ -0,0 +1,93 @@
+package bridge
+
+import (
+	"context"
+	log "github.com/pion/ion-log"
+	ion_sfu "github.com/pion/ion-sfu/pkg/sfu"
+	"github.com/pion/ion/proto/rtc"
+	"github.com/pion/webrtc/v3"
+)
+
+func candidateSetting(pc *webrtc.PeerConnection, peer *ion_sfu.PeerLocal, errCh chan<- error, Target rtc.Target) {
+	var pcCand []webrtc.ICECandidateInit // Store unsended ICECandidate
+	peer.OnIceCandidate = func(candidate *webrtc.ICECandidateInit, target int) {
+		if target != int(Target) { // detect target
+			return // I do not want other's candidate
+		}
+		if pc.CurrentRemoteDescription() == nil { // If not initialized
+			pcCand = append(pcCand, *candidate) // just store it
+			return
+		}
+		// initialized
+		tpcCand := pcCand
+		pcCand = []webrtc.ICECandidateInit{} // Clear it
+		// And add them all
+		tpcCand = append(tpcCand, *candidate)
+		for _, c := range tpcCand {
+			err := pc.AddICECandidate(c)
+			if err != nil {
+				log.Errorf("Cannot add ICECandidate: %+v", err)
+				select {
+				case errCh <- err:
+				default:
+				}
+				return
+			}
+		}
+	}
+	pc.OnICECandidate(func(candidate *webrtc.ICECandidate) {
+		// Just do it, bridgePeer can dealing with Stable state
+		if candidate == nil {
+			return
+		}
+		err := peer.Trickle(candidate.ToJSON(), int(Target))
+		if err != nil {
+			log.Errorf("Cannot Trickle: %+v", err)
+			select {
+			case errCh <- err:
+			default:
+			}
+			return
+		}
+	})
+}
+
+type bridgePeer struct {
+	peer    *ion_sfu.PeerLocal
+	pc      *webrtc.PeerConnection
+	errCh   chan error
+	ctx     context.Context
+	cancel  context.CancelFunc
+	OnClose func(err error)
+}
+
+func newPeer(pr *ion_sfu.PeerLocal, pc *webrtc.PeerConnection) bridgePeer {
+	ctx, cancel := context.WithCancel(context.Background())
+	return bridgePeer{peer: pr, pc: pc, errCh: make(chan error, 16), ctx: ctx, cancel: cancel}
+}
+
+func (p bridgePeer) logger() {
+	for {
+		select {
+		case err := <-p.errCh:
+			p.close(err)
+		case <-p.ctx.Done():
+			p.close(nil)
+		}
+	}
+}
+
+func (p bridgePeer) close(err0 error) {
+	p.cancel()
+	err := p.peer.Close()
+	if err != nil {
+		log.Errorf("Error when closing bridgePeer in publisher: %+v", err)
+	}
+	err = p.pc.Close()
+	if err != nil {
+		log.Errorf("Error when closing pc in publisher: %+v", err)
+	}
+	if p.OnClose != nil {
+		p.OnClose(err0)
+	}
+}
