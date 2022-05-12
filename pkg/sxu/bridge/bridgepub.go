29fd33cfce25b80a8ab7d7370d7f345d16619396
yindaheng98 <yindaheng98@gmail.com>
2022-05-12 17:56:50 +0800

    new publisher logic

--- pkg/sxu/bridge/bridgepub.go
+++ pkg/sxu/bridge/bridgepub.go
@@ -5,54 +5,78 @@ import (
 	ion_sfu "github.com/pion/ion-sfu/pkg/sfu"
 	"github.com/pion/ion/proto/rtc"
 	"github.com/pion/webrtc/v3"
+	"github.com/yindaheng98/dion/util"
 )
+
+type PublisherFactory struct {
+	sfu *ion_sfu.SFU
+	SID string
+}
+
+func (p PublisherFactory) NewDoor() (util.Door, error) {
+	peer := ion_sfu.NewPeer(p.sfu)
+	me, err := getSubscriberMediaEngine()
+	if err != nil {
+		log.Errorf("Cannot getPublisherMediaEngine for pc: %+v", err)
+		return nil, err
+	}
+	api := webrtc.NewAPI(webrtc.WithMediaEngine(me), webrtc.WithSettingEngine(webrtc.SettingEngine{}))
+	pc, err := api.NewPeerConnection(webrtc.Configuration{})
+	if err != nil {
+		log.Errorf("Cannot NewPeerConnection: %+v", err)
+		return nil, err
+	}
+	return Publisher{
+		bridgePeer: bridgePeer{
+			peer: peer,
+			pc:   pc,
+		},
+		sid: p.SID,
+	}, nil
+}
 
 type Publisher struct {
 	bridgePeer
+	sid string
+}
+
+func (p Publisher) Lock(OnBroken func(badGay error)) error {
+	return p.publish(p.sid, OnBroken)
+}
+
+func (p bridgePeer) Repair() bool {
+	return false
 }
 
 func NewPublisher(peer *ion_sfu.PeerLocal, pc *webrtc.PeerConnection) Publisher {
 	return Publisher{bridgePeer: newPeer(peer, pc)}
 }
 
-// Publish publish PeerConnection to PeerLocal.Subscriber
-func (p Publisher) Publish(sid string) error {
-	errCh := p.errCh
+// publish publish PeerConnection to PeerLocal.Subscriber
+func (p bridgePeer) publish(sid string, OnBroken func(badGay error)) error {
 	p.pc.OnNegotiationNeeded(func() {
 		offer, err := p.pc.CreateOffer(nil)
 		if err != nil {
 			log.Errorf("Cannot CreateOffer in pc: %+v", err)
-			select {
-			case errCh <- err:
-			default:
-			}
+			OnBroken(err)
 			return
 		}
 		err = p.pc.SetLocalDescription(offer)
 		if err != nil {
 			log.Errorf("Cannot SetLocalDescription to pc: %+v", err)
-			select {
-			case errCh <- err:
-			default:
-			}
+			OnBroken(err)
 			return
 		}
 		answer, err := p.peer.Answer(offer)
 		if err != nil {
-			log.Errorf("Cannot create Answer in bridgePeer: %+v", err)
-			select {
-			case errCh <- err:
-			default:
-			}
+			log.Errorf("Cannot create Answer in peer: %+v", err)
+			OnBroken(err)
 			return
 		}
 		err = p.pc.SetRemoteDescription(*answer)
 		if err != nil {
 			log.Errorf("Cannot SetRemoteDescription to pc: %+v", err)
-			select {
-			case errCh <- err:
-			default:
-			}
+			OnBroken(err)
 			return
 		}
 	})
@@ -66,9 +90,8 @@ func (p Publisher) Publish(sid string) error {
 		return err
 	}
 
-	candidateSetting(p.pc, p.peer, p.errCh, rtc.Target_PUBLISHER)
+	candidateSetting(p.pc, p.peer, OnBroken, rtc.Target_PUBLISHER)
 
-	go p.logger()
 	return err
 }
 
--- pkg/sxu/bridge/common.go
+++ pkg/sxu/bridge/common.go
@@ -1,14 +1,13 @@
 package bridge
 
 import (
-	"context"
 	log "github.com/pion/ion-log"
 	ion_sfu "github.com/pion/ion-sfu/pkg/sfu"
 	"github.com/pion/ion/proto/rtc"
 	"github.com/pion/webrtc/v3"
 )
 
-func candidateSetting(pc *webrtc.PeerConnection, peer *ion_sfu.PeerLocal, errCh chan<- error, Target rtc.Target) {
+func candidateSetting(pc *webrtc.PeerConnection, peer *ion_sfu.PeerLocal, OnBroken func(error), Target rtc.Target) {
 	var pcCand []webrtc.ICECandidateInit // Store unsended ICECandidate
 	peer.OnIceCandidate = func(candidate *webrtc.ICECandidateInit, target int) {
 		if target != int(Target) { // detect target
@@ -27,10 +26,7 @@ func candidateSetting(pc *webrtc.PeerConnection, peer *ion_sfu.PeerLocal, errCh 
 			err := pc.AddICECandidate(c)
 			if err != nil {
 				log.Errorf("Cannot add ICECandidate: %+v", err)
-				select {
-				case errCh <- err:
-				default:
-				}
+				OnBroken(err)
 				return
 			}
 		}
@@ -43,42 +39,18 @@ func candidateSetting(pc *webrtc.PeerConnection, peer *ion_sfu.PeerLocal, errCh 
 		err := peer.Trickle(candidate.ToJSON(), int(Target))
 		if err != nil {
 			log.Errorf("Cannot Trickle: %+v", err)
-			select {
-			case errCh <- err:
-			default:
-			}
+			OnBroken(err)
 			return
 		}
 	})
 }
 
 type bridgePeer struct {
-	peer    *ion_sfu.PeerLocal
-	pc      *webrtc.PeerConnection
-	errCh   chan error
-	ctx     context.Context
-	cancel  context.CancelFunc
-	OnClose func(err error)
+	peer *ion_sfu.PeerLocal
+	pc   *webrtc.PeerConnection
 }
 
-func newPeer(pr *ion_sfu.PeerLocal, pc *webrtc.PeerConnection) bridgePeer {
-	ctx, cancel := context.WithCancel(context.Background())
-	return bridgePeer{peer: pr, pc: pc, errCh: make(chan error, 16), ctx: ctx, cancel: cancel}
-}
-
-func (p bridgePeer) logger() {
-	for {
-		select {
-		case err := <-p.errCh:
-			p.close(err)
-		case <-p.ctx.Done():
-			p.close(nil)
-		}
-	}
-}
-
-func (p bridgePeer) close(err0 error) {
-	p.cancel()
+func (p bridgePeer) Remove() {
 	err := p.peer.Close()
 	if err != nil {
 		log.Errorf("Error when closing bridgePeer in publisher: %+v", err)
@@ -87,13 +59,10 @@ func (p bridgePeer) close(err0 error) {
 	if err != nil {
 		log.Errorf("Error when closing pc in publisher: %+v", err)
 	}
-	if p.OnClose != nil {
-		p.OnClose(err0)
-	}
 }
 
-func (p bridgePeer) Close() {
-	p.close(nil)
+func newPeer(pr *ion_sfu.PeerLocal, pc *webrtc.PeerConnection) bridgePeer {
+	return bridgePeer{peer: pr, pc: pc}
 }
 
 func (p bridgePeer) OnConnectionStateChange(f func(webrtc.PeerConnectionState)) {
