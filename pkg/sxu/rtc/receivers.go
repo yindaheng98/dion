38c2ef88753f3cc127aa3018b61cf124aca131ec
yindaheng98 <yindaheng98@gmail.com>
2022-05-16 22:06:07 +0800

    split

---
+++ pkg/sxu/rtc/receivers.go
@@ -0,0 +1,156 @@
+package rtc
+
+import (
+	"encoding/json"
+	"errors"
+	log "github.com/pion/ion-log"
+	"github.com/pion/ion/proto/rtc"
+	"github.com/pion/webrtc/v3"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"io"
+)
+
+// ↓↓↓↓↓ Copy from: https://github.com/pion/ion-sdk-go/blob/12e32a5871b905bf2bdf58bc45c2fdd2741c4f81/rtc.go ↓↓↓↓↓
+
+func (r *RTC) onSingalHandleOnce() {
+	// onSingalHandle is wrapped in a once and only started after another public
+	// method is called to ensure the user has the opportunity to register handlers
+	r.handleOnce.Do(func() {
+		err := r.onSingalHandle()
+		if r.OnError != nil {
+			r.OnError(err)
+		}
+	})
+}
+
+func (r *RTC) onSingalHandle() error {
+	for {
+		//only one goroutine for recving from stream, no need to lock
+		stream, err := r.signaller.Recv()
+		if err != nil {
+			if err == io.EOF {
+				log.Infof("[%v] WebRTC Transport Closed", r.uid)
+				if err := r.signaller.CloseSend(); err != nil {
+					log.Errorf("[%v] error sending close: %s", r.uid, err)
+				}
+				return err
+			}
+
+			errStatus, _ := status.FromError(err)
+			if errStatus.Code() == codes.Canceled {
+				if err := r.signaller.CloseSend(); err != nil {
+					log.Errorf("[%v] error sending close: %s", r.uid, err)
+				}
+				return err
+			}
+
+			log.Errorf("[%v] Error receiving RTC response: %v", r.uid, err)
+			/*
+				if r.OnError != nil {
+					r.OnError(err)
+				}
+			*/
+			return err
+		}
+
+		switch payload := stream.Payload.(type) {
+		case *rtc.Reply_Join:
+			success := payload.Join.Success
+			err := errors.New(payload.Join.Error.String())
+
+			if !success {
+				log.Errorf("[%v] [join] failed error: %v", r.uid, err)
+				return err
+			}
+			log.Infof("[%v] [join] success", r.uid)
+			/*
+				log.Infof("payload.Reply.Description=%v", payload.Join.Description)
+				sdp := webrtc.SessionDescription{
+					Type: webrtc.SDPTypeAnswer,
+					SDP:  payload.Join.Description.Sdp,
+				}
+
+				if err = r.setRemoteSDP(sdp); err != nil {
+					log.Errorf("[%v] [join] error %s", r.uid, err)
+					return err
+				}
+			*/
+		case *rtc.Reply_Description:
+			var sdpType webrtc.SDPType
+			if payload.Description.Type == "offer" {
+				sdpType = webrtc.SDPTypeOffer
+			} else {
+				sdpType = webrtc.SDPTypeAnswer
+			}
+			sdp := webrtc.SessionDescription{
+				SDP:  payload.Description.Sdp,
+				Type: sdpType,
+			}
+			if sdp.Type == webrtc.SDPTypeOffer {
+				log.Infof("[%v] [description] got offer call s.OnNegotiate sdp=%+v", r.uid, sdp)
+				err := r.negotiate(sdp)
+				if err != nil {
+					log.Errorf("error: %v", err)
+					// ↑↑↑↑↑ Copy from: https://github.com/pion/ion-sdk-go/blob/12e32a5871b905bf2bdf58bc45c2fdd2741c4f81/rtc.go ↑↑↑↑↑
+					return err
+				}
+			} else if sdp.Type == webrtc.SDPTypeAnswer {
+				log.Infof("[%v] [description] got answer call sdp=%+v, but i do not have a publisher", r.uid, sdp)
+			}
+			// ↓↓↓↓↓ Copy from: https://github.com/pion/ion-sdk-go/blob/12e32a5871b905bf2bdf58bc45c2fdd2741c4f81/rtc.go ↓↓↓↓↓
+		case *rtc.Reply_Trickle:
+			var candidate webrtc.ICECandidateInit
+			_ = json.Unmarshal([]byte(payload.Trickle.Init), &candidate)
+			log.Infof("[%v] [trickle] type=%v candidate=%v", r.uid, payload.Trickle.Target, candidate)
+			// ↑↑↑↑↑ Copy from: https://github.com/pion/ion-sdk-go/blob/12e32a5871b905bf2bdf58bc45c2fdd2741c4f81/rtc.go ↑↑↑↑↑
+			err := r.trickle(candidate, Target(payload.Trickle.Target))
+			if err != nil {
+				return err
+			}
+			// ↓↓↓↓↓ Copy from: https://github.com/pion/ion-sdk-go/blob/12e32a5871b905bf2bdf58bc45c2fdd2741c4f81/rtc.go ↓↓↓↓↓
+		case *rtc.Reply_TrackEvent:
+			/*
+				if r.OnTrackEvent == nil {
+					log.Errorf("s.OnTrackEvent == nil")
+					continue
+				}
+				var TrackInfos []*TrackInfo
+				for _, v := range payload.TrackEvent.Tracks {
+					TrackInfos = append(TrackInfos, &TrackInfo{
+						Id:        v.Id,
+						Kind:      v.Kind,
+						Muted:     v.Muted,
+						Type:      MediaType(v.Type),
+						StreamId:  v.StreamId,
+						Label:     v.Label,
+						Width:     v.Width,
+						Height:    v.Height,
+						FrameRate: v.FrameRate,
+						Layer:     v.Layer,
+					})
+				}
+				trackEvent := TrackEvent{
+					State:  TrackEvent_State(payload.TrackEvent.State),
+					Uid:    payload.TrackEvent.Uid,
+					Tracks: TrackInfos,
+				}
+
+				log.Infof("s.OnTrackEvent trackEvent=%+v", trackEvent)
+				r.OnTrackEvent(trackEvent)
+			*/
+		case *rtc.Reply_Subscription:
+			if !payload.Subscription.Success {
+				log.Errorf("suscription error: %v", payload.Subscription.Error)
+				return errors.New(payload.Subscription.Error.String())
+			}
+		case *rtc.Reply_Error:
+			log.Errorf("Request error: %v", payload.Error)
+			return errors.New(payload.Error.String())
+		default:
+			log.Errorf("Unknown RTC type!!!!%v", payload)
+		}
+	}
+}
+
+// ↑↑↑↑↑ Copy from: https://github.com/pion/ion-sdk-go/blob/12e32a5871b905bf2bdf58bc45c2fdd2741c4f81/rtc.go ↑↑↑↑↑
---
+++ pkg/sxu/rtc/senders.go
@@ -0,0 +1,126 @@
+package rtc
+
+import (
+	"encoding/json"
+	log "github.com/pion/ion-log"
+	"github.com/pion/ion/proto/rtc"
+	"github.com/pion/webrtc/v3"
+)
+
+func (r *RTC) SendJoin(sid string, uid string /*offer webrtc.SessionDescription, config map[string]string*/) error {
+	// ↓↓↓↓↓ Copy from: https://github.com/pion/ion-sdk-go/blob/12e32a5871b905bf2bdf58bc45c2fdd2741c4f81/rtc.go ↓↓↓↓↓
+	log.Infof("[C=>S] [%v] sid=%v", r.uid, sid)
+	r.Lock()
+	err := r.signaller.Send(
+		&rtc.Request{
+			Payload: &rtc.Request_Join{
+				Join: &rtc.JoinRequest{
+					Sid: sid,
+					Uid: uid,
+					// ↑↑↑↑↑ Copy from: https://github.com/pion/ion-sdk-go/blob/12e32a5871b905bf2bdf58bc45c2fdd2741c4f81/rtc.go ↑↑↑↑↑
+					Config: map[string]string{
+						"NoPublish":       "true",
+						"NoSubscribe":     "false",
+						"NoAutoSubscribe": "false",
+					},
+					// ↓↓↓↓↓ Copy from: https://github.com/pion/ion-sdk-go/blob/12e32a5871b905bf2bdf58bc45c2fdd2741c4f81/rtc.go ↓↓↓↓↓
+					/*
+						Description: &rtc.SessionDescription{
+							Target: rtc.Target_PUBLISHER,
+							Type:   "offer",
+							Sdp:    offer.SDP,
+						},
+					*/
+				},
+			},
+		},
+	)
+	r.Unlock()
+	if err != nil {
+		log.Errorf("[C=>S] [%v] err=%v", r.uid, err)
+	}
+	return err
+}
+
+// ↑↑↑↑↑ Copy from: https://github.com/pion/ion-sdk-go/blob/12e32a5871b905bf2bdf58bc45c2fdd2741c4f81/rtc.go ↑↑↑↑↑
+
+func (r *RTC) SendTrickle(candidate *webrtc.ICECandidate, target Target) error {
+	log.Debugf("[C=>S] [%v] candidate=%v target=%v", r.uid, candidate, target)
+	bytes, err := json.Marshal(candidate.ToJSON())
+	if err != nil {
+		log.Errorf("error: %v", err)
+		return err
+	}
+	// ↓↓↓↓↓ Copy from: https://github.com/pion/ion-sdk-go/blob/12e32a5871b905bf2bdf58bc45c2fdd2741c4f81/rtc.go ↓↓↓↓↓
+	r.Lock()
+	err = r.signaller.Send(
+		&rtc.Request{
+			Payload: &rtc.Request_Trickle{
+				Trickle: &rtc.Trickle{
+					Target: rtc.Target(target),
+					Init:   string(bytes),
+				},
+			},
+		},
+	)
+	r.Unlock()
+	if err != nil {
+		log.Errorf("[%v] err=%v", r.uid, err)
+	}
+	// ↑↑↑↑↑ Copy from: https://github.com/pion/ion-sdk-go/blob/12e32a5871b905bf2bdf58bc45c2fdd2741c4f81/rtc.go ↑↑↑↑↑
+	return err
+}
+
+// ↓↓↓↓↓ Copy from: https://github.com/pion/ion-sdk-go/blob/12e32a5871b905bf2bdf58bc45c2fdd2741c4f81/rtc.go ↓↓↓↓↓
+
+func (r *RTC) SendAnswer(sdp webrtc.SessionDescription) error {
+	log.Infof("[C=>S] [%v] sdp=%v", r.uid, sdp)
+	r.Lock()
+	err := r.signaller.Send(
+		&rtc.Request{
+			Payload: &rtc.Request_Description{
+				Description: &rtc.SessionDescription{
+					Target: rtc.Target_SUBSCRIBER,
+					Type:   "answer",
+					Sdp:    sdp.SDP,
+				},
+			},
+		},
+	)
+	r.Unlock()
+	if err != nil {
+		log.Errorf("[%v] err=%v", r.uid, err)
+		return err
+	}
+	return nil
+}
+
+// Subscribe to tracks
+func (r *RTC) Subscribe(trackInfos []*Subscription) error {
+	if len(trackInfos) == 0 {
+		return nil
+	}
+	var infos []*rtc.Subscription
+	for _, t := range trackInfos {
+		infos = append(infos, &rtc.Subscription{
+			TrackId:   t.TrackId,
+			Mute:      t.Mute,
+			Subscribe: t.Subscribe,
+			Layer:     t.Layer,
+		})
+	}
+
+	log.Infof("[C=>S] infos: %v", infos)
+	err := r.signaller.Send(
+		&rtc.Request{
+			Payload: &rtc.Request_Subscription{
+				Subscription: &rtc.SubscriptionRequest{
+					Subscriptions: infos,
+				},
+			},
+		},
+	)
+	return err
+}
+
+// ↑↑↑↑↑ Copy from: https://github.com/pion/ion-sdk-go/blob/12e32a5871b905bf2bdf58bc45c2fdd2741c4f81/rtc.go ↑↑↑↑↑
