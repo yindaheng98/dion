822237e111257cc63b060087d5bbc45666a961fc
yindaheng98 <yindaheng98@gmail.com>
2022-05-17 19:41:54 +0800

    modified UpPeerLocal

--- pkg/sxu/rtc/peer.go
+++ pkg/sxu/rtc/peer.go
@@ -1,29 +1,31 @@
 package rtc
 
 import (
+	log "github.com/pion/ion-log"
 	ion_sfu "github.com/pion/ion-sfu/pkg/sfu"
+	"github.com/pion/ion/proto/rtc"
 	"github.com/pion/webrtc/v3"
 )
 
 // UpPeerLocal is a local peer that only have up tracks (tracks from other nodes)
 type UpPeerLocal struct {
-	*ion_sfu.PeerLocal
+	peer *ion_sfu.PeerLocal
 }
 
 // Join the up track peer join a session, with the option NoPublish = false and NoSubscribe = true
 func (p *UpPeerLocal) Join(sid string) error {
-	return p.PeerLocal.Join(sid, "", ion_sfu.JoinConfig{
+	return p.peer.Join(sid, "", ion_sfu.JoinConfig{
 		NoPublish:   false,
 		NoSubscribe: true,
 	})
 }
 
-func (p *UpPeerLocal) SetRemoteDescription(desc webrtc.SessionDescription) error {
-	return p.Publisher().PeerConnection().SetRemoteDescription(desc)
+func (p *UpPeerLocal) Answer(desc webrtc.SessionDescription) (*webrtc.SessionDescription, error) {
+	return p.peer.Answer(desc)
 }
 
 func (p *UpPeerLocal) AddICECandidate(candidate webrtc.ICECandidateInit) error {
-	return p.Publisher().AddICECandidate(candidate)
+	return p.peer.Trickle(candidate, int(rtc.Target_PUBLISHER))
 }
 
 /*
@@ -33,22 +35,20 @@ func (p *UpPeerLocal) PeerConnection() *webrtc.PeerConnection {
 }
 */
 
-func (p *UpPeerLocal) OnICECandidate(f func(c *webrtc.ICECandidate)) {
-	p.Publisher().OnICECandidate(f)
-}
-
-func (p *UpPeerLocal) CurrentRemoteDescription() *webrtc.SessionDescription {
-	return p.Publisher().PeerConnection().CurrentRemoteDescription()
+func (p *UpPeerLocal) OnICECandidate(f func(c *webrtc.ICECandidateInit)) {
+	p.peer.OnIceCandidate = func(init *webrtc.ICECandidateInit, target int) {
+		if rtc.Target(target) == rtc.Target_PUBLISHER {
+			f(init)
+		}
+	}
 }
 
 func (p *UpPeerLocal) GetStats() webrtc.StatsReport {
-	return p.Publisher().PeerConnection().GetStats()
+	return p.peer.Publisher().PeerConnection().GetStats()
 }
 
-func (p *UpPeerLocal) CreateAnswer(options *webrtc.AnswerOptions) (webrtc.SessionDescription, error) {
-	return p.Publisher().PeerConnection().CreateAnswer(options)
-}
-
-func (p *UpPeerLocal) SetLocalDescription(sdp webrtc.SessionDescription) error {
-	return p.Publisher().PeerConnection().SetLocalDescription(sdp)
+func (p *UpPeerLocal) Close() {
+	if err := p.peer.Close(); err != nil {
+		log.Errorf("Error when closing: %+v", err)
+	}
 }
