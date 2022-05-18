daa0161e8c45c937fe4be3d692c36ff213267ee9
yindaheng98 <yindaheng98@gmail.com>
2022-05-18 14:25:17 +0800

    move it

--- pkg/sxu/rtc/rtc.go
+++ pkg/sxu/rtc/rtc.go
@@ -5,7 +5,6 @@ import (
 	log "github.com/pion/ion-log"
 	"github.com/pion/ion/proto/rtc"
 	"github.com/pion/webrtc/v3"
-	pb "github.com/yindaheng98/dion/proto"
 	"sync"
 )
 
@@ -102,38 +101,4 @@ func (r *RTC) negotiate(offer webrtc.SessionDescription) error {
 		return err
 	}
 	return err
-}
-
-var Layers = map[pb.Subscription_Layer]string{
-	pb.Subscription_Q: "q",
-	pb.Subscription_H: "h",
-	pb.Subscription_F: "f",
-}
-
-func (r *RTC) Update(tracks []*pb.Subscription) error {
-	trackInfos := make([]*Subscription, len(tracks))
-	for i, track := range tracks {
-		trackInfos[i] = &Subscription{
-			TrackId:   track.TrackId,
-			Mute:      track.Mute,
-			Subscribe: true,
-			Layer:     Layers[track.Layer],
-		}
-	}
-	return r.Subscribe(trackInfos)
-}
-
-func (r *RTC) IsSame(tracks []*pb.Subscription) bool {
-	temp := map[string]*webrtc.TrackRemote{}
-	for _, t := range r.peer.peer.Publisher().PublisherTracks() {
-		temp[t.Track.ID()] = t.Track
-	}
-	for _, sub := range tracks {
-		if t, ok := temp[sub.TrackId]; ok {
-			if !TrackSame(sub, t) {
-				return false
-			}
-		}
-	}
-	return true
 }
--- pkg/sxu/rtc/tracksame.go
+++ pkg/sxu/rtc/tracksame.go
@@ -5,6 +5,12 @@ import (
 	pb "github.com/yindaheng98/dion/proto"
 )
 
+var Layers = map[pb.Subscription_Layer]string{
+	pb.Subscription_Q: "q",
+	pb.Subscription_H: "h",
+	pb.Subscription_F: "f",
+}
+
 func TrackSame(t1 *pb.Subscription, t2 *webrtc.TrackRemote) bool {
 	if t1.TrackId != t2.ID() {
 		return false
@@ -13,4 +19,39 @@ func TrackSame(t1 *pb.Subscription, t2 *webrtc.TrackRemote) bool {
 		return false
 	}
 	return true
+}
+
+func (r *RTC) update(tracks []*pb.Subscription) error {
+	trackInfos := make([]*Subscription, len(tracks))
+	for i, track := range tracks {
+		trackInfos[i] = &Subscription{
+			TrackId:   track.TrackId,
+			Mute:      track.Mute,
+			Subscribe: true,
+			Layer:     Layers[track.Layer],
+		}
+	}
+	return r.Subscribe(trackInfos)
+}
+
+func (r *RTC) isSame(tracks []*pb.Subscription) bool {
+	temp := map[string]*webrtc.TrackRemote{}
+	for _, t := range r.peer.peer.Publisher().PublisherTracks() {
+		temp[t.Track.ID()] = t.Track
+	}
+	for _, sub := range tracks {
+		if t, ok := temp[sub.TrackId]; ok {
+			if !TrackSame(sub, t) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func (r *RTC) Update(tracks []*pb.Subscription) error {
+	if !r.isSame(tracks) {
+		return r.update(tracks)
+	}
+	return nil
 }
