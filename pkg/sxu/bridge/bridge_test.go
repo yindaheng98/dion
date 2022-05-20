0d374f1b1226f27694473c03a22a2e9638f6e78a
yindaheng98 <yindaheng98@gmail.com>
2022-05-20 18:33:27 +0800

    testing bridge

--- pkg/sxu/bridge/bridge_test.go
+++ pkg/sxu/bridge/bridge_test.go
@@ -3,6 +3,7 @@ package bridge
 import (
 	log "github.com/pion/ion-log"
 	ion_sfu "github.com/pion/ion-sfu/pkg/sfu"
+	pb "github.com/yindaheng98/dion/proto"
 	"github.com/yindaheng98/dion/util"
 	"os"
 	"os/signal"
@@ -15,45 +16,25 @@ const YourName = "stupid2"
 
 func TestEntrance(t *testing.T) {
 	confFile := "D:\\Documents\\MyPrograms\\dion\\pkg\\sxu\\sfu.toml"
-	ffmpeg := "D:\\Documents\\MyPrograms\\ffmpeg"
-	testvideo := "size=1280x720:rate=30"
+	ffmpeg := "D:\\Documents\\MyPrograms\\ffmpeg.exe"
 
 	conf := readConf(confFile)
-
-	ffmpegOut := makeVideo(ffmpeg, testvideo)
 
 	log.Init(conf.Log.Level)
 	log.Infof("--- starting sfu node ---")
 
 	iSFU := ion_sfu.NewSFU(conf.Config)
 
-	exitFact := NewPublisherFactory(iSFU)
-	exitDoor, err := exitFact.NewDoor()
-	if err != nil {
-		panic(err)
-	}
-	exit := exitDoor.(Publisher)
-	err = exit.Lock(SID(YourName), func(badGay error) {
-		log.Errorf("bad gay comes: %+v", badGay)
-		panic(badGay)
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	ent := EntranceFactory{
-		SubscriberFactory: SubscriberFactory{
-			sfu: iSFU,
-		},
-		exit: exit,
-		road: NewSimpleFFmpegProcessor(ffmpeg),
-	}
-	entdog := util.NewWatchDogWithUnblockedDoor(ent)
-	entdog.Watch(SID(MyName))
+	br := NewBridgeFactory(iSFU, NewSimpleFFmpegIVFProcessorFactory(ffmpeg))
+	brdog := util.NewWatchDogWithUnblockedDoor(br)
+	brdog.Watch(ProceedTrackParam{ProceedTrack: &pb.ProceedTrack{
+		DstSessionId:     YourName,
+		SrcSessionIdList: []string{MyName},
+	}})
 
 	<-time.After(5 * time.Second)
 
-	pub := NewTestPublisherFactory(ffmpegOut, iSFU)
+	pub := NewSimpleFFmpegTestsrcPublisher(ffmpeg, iSFU)
 	pubdog := util.NewWatchDogWithUnblockedDoor(pub)
 	pubdog.Watch(SID(MyName))
 
