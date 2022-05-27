b923be6e5e0aaddaa3825cee4ff8786fcef9a95b
yindaheng98 <yindaheng98@gmail.com>
2022-05-27 22:01:29 +0800

    bug fix

--- pkg/sxu/syncer/config.go
+++ pkg/sxu/syncer/config.go
@@ -5,5 +5,5 @@ import "github.com/yindaheng98/dion/config"
 type Config config.Common
 
 func (c *Config) Load(file string) error {
-	return config.Load(file, c)
+	return config.LoadFromToml(file, c)
 }
