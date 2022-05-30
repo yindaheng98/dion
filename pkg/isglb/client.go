5f208da3a91114b3d5d72191547ff25c9d4649fa
yindaheng98 <yindaheng98@gmail.com>
2022-05-30 23:21:54 +0800

    enhance the client

--- pkg/isglb/client.go
+++ pkg/isglb/client.go
@@ -2,82 +2,186 @@ package isglb
 
 import (
 	"context"
-	"fmt"
-	"github.com/yindaheng98/dion/config"
-	"io"
-
 	log "github.com/pion/ion-log"
 	sdk "github.com/pion/ion-sdk-go"
 	"github.com/pion/ion/pkg/ion"
+	"github.com/yindaheng98/dion/config"
 	pb "github.com/yindaheng98/dion/proto"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
+	"github.com/yindaheng98/dion/util"
 	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"
+	"sync/atomic"
 )
 
 type ISGLBClient struct {
 	sdk.Service
-	connected   bool
-	isglbClient pb.ISGLBClient
-	isglbStream pb.ISGLB_SyncSFUClient
+	node       *ion.Node
+	peerNID    string
+	parameters map[string]interface{}
+	ctxTop     context.Context
+	cancelTop  context.CancelFunc
 
-	ctx    context.Context
-	cancel context.CancelFunc
+	connected         atomic.Value
+	msgReadLoopExec   *util.SingleExec
+	sendSFUStatusExec *util.SingleLatestExec
+	reconnectExec     *util.SingleWaitExec
+
+	client     pb.ISGLB_SyncSFUClient
+	cancelLast context.CancelFunc
 
 	Metadata metadata.MD
-	grpcConn grpc.ClientConnInterface
 
 	OnSFUStatusRecv func(s *pb.SFUStatus)
 }
 
 func NewISGLBClient(node *ion.Node, peerNID string, parameters map[string]interface{}) *ISGLBClient {
-	ncli, err := node.NewNatsRPCClient(config.ServiceISGLB, peerNID, parameters)
-	if err != nil {
-		log.Errorf("error: %v", err)
-		return nil
-	}
+	ctx, cancal := context.WithCancel(context.Background())
 	c := &ISGLBClient{
-		grpcConn: ncli,
+		node:              node,
+		peerNID:           peerNID,
+		parameters:        parameters,
+		ctxTop:            ctx,
+		cancelTop:         cancal,
+		msgReadLoopExec:   util.NewSingleExec(),
+		sendSFUStatusExec: &util.SingleLatestExec{},
+		reconnectExec:     &util.SingleWaitExec{},
 	}
+	c.connected.Store(false)
 	return c
 }
 
-func (c *ISGLBClient) SendSyncRequest(r *pb.SyncRequest) error {
-	err := c.isglbStream.Send(r)
-	if err != nil {
-		return err
+// doWithStream do something with c.stream
+func (c *ISGLBClient) doWithClient(op func(client pb.ISGLB_SyncSFUClient) error) bool {
+	select {
+	case <-c.ctxTop.Done(): // should exit now?
+		return true // exit
+	default:
 	}
-	return nil
+	if c.client == nil { // no client?
+		c.reconnect() // make a client
+		return false
+	}
+	err := op(c.client) // has client? just do it
+	if err != nil {     // error? should reconnect
+		select {
+		case <-c.ctxTop.Done(): // should exit now?
+			return true // do not reconnect
+		default:
+		}
+		c.reconnect() // reconnect
+		return false
+	}
+	return true
 }
 
-func (c *ISGLBClient) isglbReadLoop(cancelFunc context.CancelFunc) {
+func (c *ISGLBClient) msgReadLoop() {
 	for {
-		s, err := c.isglbStream.Recv() // Receive a SyncRequest
+		select {
+		case <-c.ctxTop.Done(): // should exit now?
+			return // exit
+		default:
+		}
+		// receive msg
+		c.doWithClient(func(client pb.ISGLB_SyncSFUClient) error {
+			s, err := client.Recv() // Receive a SyncRequest
+			if err != nil {
+				log.Errorf("%v SFU status receive error %+v", err)
+				return err
+			}
+			c.OnSFUStatusRecv(s)
+			return nil
+		})
+	}
+}
+
+func (c *ISGLBClient) reconnect() {
+	// c.Do: 如果当前正在重连，就等待重连完成；如果当前不在重连，就开始重连直到完成
+	c.reconnectExec.Do(func() {
+		c.connected.Store(false)
+		log.Infof("ISGLBClient connecting......")
+
+		if c.cancelLast != nil {
+			c.cancelLast() // cancel the last stream
+		}
+
+		conn, err := c.node.NewNatsRPCClient(config.ServiceISGLB, c.peerNID, c.parameters)
 		if err != nil {
-			if err == io.EOF {
-				cancelFunc()
-				return
-			}
-			errStatus, _ := status.FromError(err)
-			if errStatus.Code() == codes.Canceled {
-				cancelFunc()
-				return
-			}
-			log.Errorf("%v SFU status receive error %d", fmt.Errorf(errStatus.Message()), errStatus.Code())
-			cancelFunc()
+			log.Errorf("cannot NewNatsRPCClient: %v", err)
 			return
 		}
-		c.OnSFUStatusRecv(s)
+
+		ctx, cancel := context.WithCancel(c.ctxTop)
+		c.cancelLast = cancel
+
+		ctx = metadata.NewOutgoingContext(ctx, c.Metadata)
+		c.client, err = pb.NewISGLBClient(conn).SyncSFU(ctx)
+		if err != nil {
+			log.Errorf("cannot NewISGLBClient: %v", err)
+			return
+		}
+
+		c.msgReadLoopExec.Do(c.msgReadLoop)
+
+		c.connected.Store(true)
+		log.Infof("ISGLBClient connected!")
+	})
+}
+
+// SendQualityReport send the report, maybe lose when cannot connect
+func (c *ISGLBClient) SendQualityReport(report *pb.QualityReport) {
+	c.doWithClient(func(client pb.ISGLB_SyncSFUClient) error {
+		err := client.Send(&pb.SyncRequest{Request: &pb.SyncRequest_Report{Report: report}})
+		if err != nil {
+			log.Errorf("QualityReport send error: %+v", err)
+			return err
+		}
+		return nil
+	})
+}
+
+// SendSFUStatus send the SFUStatus, if there is a new status should be send, the last send will be canceled
+func (c *ISGLBClient) SendSFUStatus(status *pb.SFUStatus) {
+	c.sendSFUStatusExec.Do(func(ctx context.Context) {
+		for {
+			select {
+			case <-c.ctxTop.Done():
+				return
+			case <-ctx.Done():
+				return
+			default:
+			}
+			ok := c.doWithClient(func(client pb.ISGLB_SyncSFUClient) error {
+				err := client.Send(&pb.SyncRequest{Request: &pb.SyncRequest_Status{Status: status}})
+				if err != nil {
+					log.Errorf("SFUStatus send error: %+v", err)
+					return err
+				}
+				return nil
+			})
+			if ok {
+				return
+			}
+		}
+	})
+}
+
+func (c *ISGLBClient) SendSyncRequest(r *pb.SyncRequest) {
+	select {
+	case <-c.ctxTop.Done():
+		return
+	default:
+	}
+	if c.client == nil {
+		c.reconnect()
+	} else {
+		err := c.client.Send(r)
+		if err != nil {
+			c.reconnect()
+		}
 	}
 }
 
-// ↓↓↓↓↓ COPY FROM https://github.com/pion/ion-sdk-go/blob/34fbe58bfa24a62a2ced716c2608a0a9fb2dbae0/room.go ↓↓↓↓↓
-
 func (c *ISGLBClient) Close() {
-	c.cancel()
-	_ = c.isglbStream.CloseSend()
-	log.Infof("Close ok")
+	c.cancelTop()
 }
 
 func (c *ISGLBClient) Name() string {
@@ -85,23 +189,9 @@ func (c *ISGLBClient) Name() string {
 }
 
 func (c *ISGLBClient) Connect() {
-	var err error
-	c.ctx, c.cancel = context.WithCancel(context.Background())
-	c.ctx = metadata.NewOutgoingContext(c.ctx, c.Metadata)
-	c.isglbClient = pb.NewISGLBClient(c.grpcConn)
-	c.isglbStream, err = c.isglbClient.SyncSFU(c.ctx)
-
-	if err != nil {
-		log.Errorf("error: %v", err)
-		return
-	}
-	go c.isglbReadLoop(c.cancel)
-	c.connected = true
-	log.Infof("ISGLBClient.Connect!")
+	c.reconnect()
 }
 
 func (c *ISGLBClient) Connected() bool {
-	return c.connected
+	return c.connected.Load().(bool)
 }
-
-// ↑↑↑↑↑ COPY FROM https://github.com/pion/ion-sdk-go/blob/34fbe58bfa24a62a2ced716c2608a0a9fb2dbae0/room.go ↑↑↑↑↑
---
+++ util/lock.go
@@ -0,0 +1,74 @@
+package util
+
+import (
+	"context"
+	"sync"
+)
+
+type SingleWaitExec struct {
+	running bool
+	sync.WaitGroup
+	mu sync.Mutex
+}
+
+// Do : do an operation.
+// If nothing is running, then run it
+// If something is running, then just wait it
+func (l *SingleWaitExec) Do(op func()) {
+	l.mu.Lock()
+	if !l.running { // nothing is running, I should run it
+		l.running = true
+		l.Add(1)
+		defer l.Done()
+		l.mu.Unlock()
+		op()
+	} else { // means something is doing, I should wait it
+		l.mu.Unlock()
+		l.Wait()
+	}
+}
+
+type SingleExec struct {
+	running chan struct{}
+}
+
+func NewSingleExec() *SingleExec {
+	c := make(chan struct{}, 1)
+	c <- struct{}{}
+	return &SingleExec{running: c}
+}
+
+// Do : do an operation.
+// If nothing is running, then run it
+// If something is running, then just exit
+func (l *SingleExec) Do(op func()) {
+	go l.do(op)
+}
+func (l *SingleExec) do(op func()) {
+	select {
+	case <-l.running:
+		op()
+		l.running <- struct{}{}
+	default:
+		return
+	}
+}
+
+type SingleLatestExec struct {
+	mu         sync.Mutex
+	cancelLast context.CancelFunc
+}
+
+// Do : do an operation.
+// If nothing is running, then run it
+// If something is running, then stop the last and run the new
+func (l *SingleLatestExec) Do(op func(ctx context.Context)) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	if l.cancelLast != nil {
+		l.cancelLast()
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	go op(ctx)
+	l.cancelLast = cancel
+}
