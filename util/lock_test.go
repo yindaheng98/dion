e73b6bb8351bbf392f8182f44140592738d2ea66
yindaheng98 <yindaheng98@gmail.com>
2022-05-31 10:43:37 +0800

    test the SingleLatestExec

--- util/lock_test.go
+++ util/lock_test.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	"context"
+	"math/rand"
 	"testing"
 	"time"
 )
@@ -12,11 +14,37 @@ func TestSingleExec(t *testing.T) {
 			e.Do(func() {
 				for j := 0; j < 5; j++ {
 					t.Logf("%d is running %d\n", i, j)
-					<-time.After(1 * time.Second)
+					<-time.After(time.Duration(rand.Int31n(1000)) * time.Millisecond)
 				}
 			})
 		}(i)
-		<-time.After(100 * time.Millisecond)
+		<-time.After(time.Duration(rand.Int31n(100)) * time.Millisecond)
 	}
 	<-time.After(10 * time.Second)
+}
+
+func TestSingleLatestExec(t *testing.T) {
+	e := SingleLatestExec{}
+	for i := 0; i < 100; i++ {
+		go func(i int) {
+			e.Do(func(ctx context.Context) {
+				for j := 0; j < 5; j++ {
+					select {
+					case <-ctx.Done():
+						t.Logf("%d exit\n", i)
+						return
+					default:
+					}
+					t.Logf("%d is running %d\n", i, j)
+					select {
+					case <-ctx.Done():
+						t.Logf("%d exit\n", i)
+						return
+					case <-time.After(time.Duration(rand.Int31n(500)) * time.Millisecond):
+					}
+				}
+			})
+		}(i)
+		<-time.After(time.Duration(rand.Int31n(1000)) * time.Millisecond)
+	}
 }
