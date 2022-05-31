408e3b18d495bc92572a582e0a5735b134ad4129
yindaheng98 <yindaheng98@gmail.com>
2022-05-31 15:10:26 +0800

    new struct SetMap

---
+++ util/setmap.go
@@ -0,0 +1,35 @@
+package util
+
+type SetMap[K, V comparable] struct {
+	m map[K]map[V]struct{}
+}
+
+func NewSetMap[K, V comparable]() SetMap[K, V] {
+	return SetMap[K, V]{make(map[K]map[V]struct{})}
+}
+
+func (m SetMap[K, V]) Add(id K, value V) {
+	if set, ok := m.m[id]; ok {
+		set[value] = struct{}{}
+	} else {
+		m.m[id] = map[V]struct{}{value: {}}
+	}
+}
+
+func (m SetMap[K, V]) Remove(id K, value V) {
+	if set, ok := m.m[id]; ok {
+		delete(set, value)
+		if len(set) <= 0 {
+			delete(m.m, id)
+		}
+	}
+}
+
+func (m SetMap[K, V]) Exists(id K, value V) bool {
+	var set map[V]struct{}
+	var ok bool
+	if set, ok = m.m[id]; ok {
+		_, ok = set[value]
+	}
+	return ok
+}
---
+++ util/setmap_test.go
@@ -0,0 +1,30 @@
+package util
+
+import "testing"
+
+func TestSetMap(t *testing.T) {
+	sm := NewSetMap[string, int]()
+	sm.Add("1", 1)
+	sm.Add("1", 2)
+	sm.Add("1", 3)
+	sm.Add("2", 1)
+	sm.Add("2", 2)
+	sm.Add("2", 3)
+	t.Log(sm.Exists("2", 1))
+	t.Log(sm.Exists("1", 3))
+	sm.Remove("1", 3)
+	sm.Remove("2", 1)
+	t.Log(sm.Exists("2", 1))
+	t.Log(sm.Exists("1", 3))
+	t.Log(sm.Exists("2", 2))
+	sm.Remove("2", 1)
+	t.Log(sm.Exists("2", 2))
+	sm.Remove("2", 2)
+	t.Log(sm.Exists("2", 2))
+	sm.Remove("2", 3)
+	sm.Add("2", 1)
+	t.Log(sm.Exists("2", 2))
+	sm.Add("2", 2)
+	t.Log(sm.Exists("2", 2))
+	sm.Add("2", 3)
+}
